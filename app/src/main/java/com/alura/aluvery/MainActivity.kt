098b8191ee7fb62b5c57b1842d187059b9ff45ea
Eduardo Britto <eduardoobritto@gmail.com>
2022-10-22 18:01:50 +0200

    Finishing ProductItem with some small adjustments.

--- app/src/main/java/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/com/alura/aluvery/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,8 +16,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alura.aluvery.ui.theme.AluveryTheme
 import com.alura.aluvery.ui.theme.Purple500
 import com.alura.aluvery.ui.theme.Teal200
@@ -37,32 +42,55 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column(
-        Modifier.height(250.dp)
-            .width(200.dp)
-    ) {
-        Box(
-            Modifier
-                .height(100.dp)
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Purple500,
-                            Teal200
+    Surface(shape= RoundedCornerShape(15.dp), elevation = 4.dp) {
+        Column(
+            Modifier.heightIn(min = 250.dp,300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500,
+                                Teal200
+                            )
                         )
                     )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = "Picture of the product",
+                    Modifier.size(imageSize)
+                        .offset(y = imageSize/2) // Half of the height
+                        .clip(CircleShape)
+                        .align(Alignment.BottomCenter)
                 )
-                .fillMaxWidth()
-        )
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_background),
-            contentDescription = "Picture of the product",
-            Modifier.size(100.dp)
-                .offset(y = (-50).dp)
-                .clip(CircleShape)
-                .align(Alignment.CenterHorizontally)
-        )
-        Text("Texto 1")
-        Text("Texto 2")
+            }
+            Spacer(Modifier.height(imageSize/2))
+            Column(
+                Modifier.padding(16.dp),
+            ) {
+                Text(
+                    LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    "R$ 14,99",
+                    Modifier.padding(top= 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+
+        }
     }
 }
+
+
