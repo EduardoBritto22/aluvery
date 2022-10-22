1b25c8a74df2083b68e1d3593edce91d13b5f0e2
Eduardo Britto <eduardoobritto@gmail.com>
2022-10-22 18:57:38 +0200

    Add a new Composable for a kit (small challenge to practice)

--- app/src/main/java/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/com/alura/aluvery/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -22,9 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.alura.aluvery.ui.theme.AluveryTheme
-import com.alura.aluvery.ui.theme.Purple500
-import com.alura.aluvery.ui.theme.Teal200
+import com.alura.aluvery.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,19 +31,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    ProductItem()
+                    KitProductItem()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Surface(shape= RoundedCornerShape(15.dp), elevation = 4.dp) {
+    Surface(
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp
+    ) {
         Column(
-            Modifier.heightIn(min = 250.dp,300.dp)
+            Modifier.heightIn(min = 250.dp, 300.dp)
                 .width(200.dp)
         ) {
             val imageSize = 100.dp
@@ -65,12 +66,12 @@ fun ProductItem() {
                     painter = painterResource(R.drawable.ic_launcher_background),
                     contentDescription = "Picture of the product",
                     Modifier.size(imageSize)
-                        .offset(y = imageSize/2) // Half of the height
+                        .offset(y = imageSize / 2) // Half of the height
                         .clip(CircleShape)
                         .align(Alignment.BottomCenter)
                 )
             }
-            Spacer(Modifier.height(imageSize/2))
+            Spacer(Modifier.height(imageSize / 2))
             Column(
                 Modifier.padding(16.dp),
             ) {
@@ -83,7 +84,7 @@ fun ProductItem() {
                 )
                 Text(
                     "R$ 14,99",
-                    Modifier.padding(top= 8.dp),
+                    Modifier.padding(top = 8.dp),
                     fontSize = 14.sp,
                     fontWeight = FontWeight(400)
                 )
@@ -93,4 +94,58 @@ fun ProductItem() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun KitProductItem() {
+    Surface(
+        shape = RoundedCornerShape(10.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            Modifier.height(200.dp)
+                .widthIn(300.dp, 350.dp)
+        ) {
+            Box(
+                Modifier.fillMaxHeight()
+                    .width(100.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Purple700,
+                                Purple200
+                            )
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = "Picture of the product kit",
+                    Modifier.size(100.dp)
+                        .offset(50.dp)
+                        .clip(CircleShape)
+                        .align(Alignment.CenterEnd)
+                        .border(
+                            width = 2.dp,
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Purple200,
+                                    Purple700
+                                )
+                            ),
+                            shape = CircleShape
+                        )
+                )
+            }
+            Spacer(Modifier.width(50.dp))
+            Text(
+                LoremIpsum(20).values.first(),
+                Modifier.padding(20.dp)
+                    .align(Alignment.CenterVertically),
+                maxLines = 6,
+                overflow = TextOverflow.Ellipsis,
+                lineHeight = 20.sp
+            )
 
+        }
+    }
+}
