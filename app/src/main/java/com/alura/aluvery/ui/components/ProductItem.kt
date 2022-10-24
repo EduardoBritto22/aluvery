ddce31c1c5d98ac61a6538f86def7f9a2404105c
Eduardo Britto <eduardoobritto@gmail.com>
2022-10-24 17:27:10 +0200

    Add product card component

--- app/src/main/java/com/alura/aluvery/ui/components/ProductItem.kt
+++ app/src/main/java/com/alura/aluvery/ui/components/ProductItem.kt
@@ -30,59 +30,63 @@ import com.alura.aluvery.ui.theme.AluveryTheme
 import java.math.BigDecimal
 
 @Composable
-fun ProductItem(product: Product) {
+fun ProductItem(
+    product: Product,
+    modifier: Modifier = Modifier,
+) {
     Surface(
-            shape = RoundedCornerShape(15.dp),
-            elevation = 4.dp
+        modifier,
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp
     ) {
         Column(
-                Modifier.heightIn(min = 250.dp, 300.dp)
-                        .width(200.dp)
+            Modifier.heightIn(min = 250.dp, 300.dp)
+                .width(200.dp)
         ) {
             val imageSize = 100.dp
             Box(
-                    Modifier
-                            .height(imageSize)
-                            .background(
-                                    brush = Brush.horizontalGradient(
-                                            colors = listOf(
-                                                    MaterialTheme.colors.primary,
-                                                    MaterialTheme.colors.secondary
-                                            )
-                                    )
+                Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                MaterialTheme.colors.primary,
+                                MaterialTheme.colors.secondary
                             )
-                            .fillMaxWidth()
+                        )
+                    )
+                    .fillMaxWidth()
             ) {
                 AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current)
-                            .data(product.image)
-                            .crossfade(true)
-                            .build(),
-                        contentDescription = "Picture of the product",
-                        Modifier.size(imageSize)
-                                .offset(y = imageSize / 2) // Half of the height
-                                .clip(CircleShape)
-                                .align(Alignment.BottomCenter),
-                        contentScale = ContentScale.Crop,
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(product.image)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "Picture of the product",
+                    Modifier.size(imageSize)
+                        .offset(y = imageSize / 2) // Half of the height
+                        .clip(CircleShape)
+                        .align(Alignment.BottomCenter),
+                    contentScale = ContentScale.Crop,
                     placeholder = painterResource(R.drawable.placeholder)
                 )
             }
             Spacer(Modifier.height(imageSize / 2))
             Column(
-                    Modifier.padding(16.dp),
+                Modifier.padding(16.dp),
             ) {
                 Text(
-                        product.name,
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight(700),
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
+                    product.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
-                        product.price.toBrazilianCurrency(),
-                        Modifier.padding(top = 8.dp),
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight(400)
+                    product.price.toBrazilianCurrency(),
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
                 )
             }
 
@@ -96,10 +100,11 @@ private fun ProductItemPreview() {
     AluveryTheme {
         Surface {
             ProductItem(
-                    Product(
-                            name = LoremIpsum(50).values.first(),
-                            price = BigDecimal("14.99")
-                    )
+                Product(
+                    name = LoremIpsum(50).values.first(),
+                    price = BigDecimal("14.99"),
+                    description = null
+                )
             )
         }
     }
