8a1d582a817627f232c41967ed36b55b26f3b985
Eduardo Britto <eduardoobritto@gmail.com>
2022-10-24 17:49:25 +0200

    Change HomeScreen to be able to show the searched products

--- app/src/main/java/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.alura.aluvery.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
@@ -14,27 +15,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alura.aluvery.model.Product
+import com.alura.aluvery.sampledata.listOfProducts
 import com.alura.aluvery.sampledata.sampleSections
+import com.alura.aluvery.ui.components.CardProductItem
 import com.alura.aluvery.ui.components.ProductSection
 import com.alura.aluvery.ui.theme.AluveryTheme
 
 @Composable
 fun HomeScreen(
-    sections: Map<String, List<Product>>
+    sections: Map<String, List<Product>>,
+    searchText: String = ""
 ) {
     Column {
 
-        var text by remember { mutableStateOf("") }
+        var text by remember { mutableStateOf(searchText) }
         OutlinedTextField(
             value = text,
             onValueChange = { newValue ->
                 text = newValue
             },
-            Modifier.padding(
-                start = 16.dp,
-                top = 16.dp,
-                end = 16.dp
-            )
+            Modifier.padding(16.dp)
                 .fillMaxWidth(),
             shape = RoundedCornerShape(100),
             leadingIcon = {
@@ -55,15 +55,24 @@ fun HomeScreen(
             Modifier
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(vertical = 16.dp),
+            contentPadding = PaddingValues(bottom = 16.dp),
         ) {
-            for (section in sections) {
-                val title = section.key
-                val products = section.value
-                item {
-                    ProductSection(
-                        title = title,
-                        products = products
+            if(text.isBlank()){
+                for (section in sections) {
+                    val title = section.key
+                    val products = section.value
+                    item {
+                        ProductSection(
+                            title = title,
+                            products = products
+                        )
+                    }
+                }
+            } else {
+                items(listOfProducts){ p->
+                    CardProductItem(
+                        product = p,
+                        Modifier.padding(horizontal = 16.dp)
                     )
                 }
             }
@@ -77,6 +86,18 @@ private fun HomeScreenPreview() {
     AluveryTheme {
         Surface {
             HomeScreen(sampleSections)
+        }
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreviewWithSearchText() {
+    AluveryTheme {
+        Surface {
+            HomeScreen(
+                sampleSections,
+                searchText = "a",
+            )
         }
     }
 }
