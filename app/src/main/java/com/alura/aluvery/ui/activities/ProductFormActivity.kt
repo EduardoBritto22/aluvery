4c3faa4d4db02589f2bd6512768b674e5d17b72b
Eduardo Britto <eduardoobritto@gmail.com>
2022-12-26 00:38:22 +0100

    Add an image preview Add scroll to the view (with fix to scroll with keyboard)

--- app/src/main/java/com/alura/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/alura/aluvery/ui/activities/ProductFormActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.alura.aluvery.R
 import com.alura.aluvery.model.Product
 import com.alura.aluvery.ui.theme.AluveryTheme
@@ -38,17 +43,31 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-
+        Spacer(modifier = Modifier)
         Text(
             text = stringResource(R.string.product_form_title),
             Modifier.fillMaxWidth(),
             fontSize = 28.sp
         )
+
         var url by remember {
             mutableStateOf("")
+        }
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .heightIn(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
         }
         TextField(
             value = url,
@@ -120,6 +139,7 @@ fun ProductFormScreen() {
         }, Modifier.fillMaxWidth()) {
             Text(text = "Save")
         }
+        Spacer(modifier = Modifier)
     }
 }
 
