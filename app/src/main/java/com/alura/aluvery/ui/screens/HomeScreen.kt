099a026705b7830b141206b8ef721d90325ad84f
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-27 09:35:12 +0100

    - Implementing the StateFlow to handle HomeScreen UI changes with the source of data (dao)

--- app/src/main/java/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,8 +24,7 @@ fun HomeScreen(
     viewModel: HomeScreenViewModel
 ) {
 
-    val state = viewModel.uiState
-
+    val state by viewModel.uiState.collectAsState()
     HomeScreen(state = state)
 }
 
--- app/src/main/java/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
@@ -1,36 +1,55 @@
 package com.alura.aluvery.ui.viewmodels
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.alura.aluvery.dao.ProductDao
 import com.alura.aluvery.model.Product
 import com.alura.aluvery.sampledata.listOfProducts
 import com.alura.aluvery.sampledata.sampleCandies
 import com.alura.aluvery.sampledata.sampleDrinks
 import com.alura.aluvery.ui.states.HomeScreenUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class HomeScreenViewModel : ViewModel() {
 
     private val dao = ProductDao()
 
 
-    var uiState: HomeScreenUiState by mutableStateOf(HomeScreenUiState(
-        sections = mapOf(
-            "All products" to dao.products(),
-            "Promotions" to sampleDrinks + sampleCandies,
-            "Drinks" to sampleDrinks,
-            "Candies" to sampleCandies
-        ),
-        onSearchChange = {
-            // Como o uiState é um MutableState, para que haja um evento de mudança, precisamos mudar a propria instancia do objeto.
-            // Para fazer isso nos reatribuimos a referencia do uiState mas pegando uma copia do mesmo e modificando apenas o que foi modificado em onSearchChange
-            uiState = uiState.copy(searchText = it, searchedProducts = searchedProducts(it))
-        }
-    ))
-        private set
+    private val _uiState: MutableStateFlow<HomeScreenUiState> = MutableStateFlow(
+        HomeScreenUiState()
+    )
 
+    val uiState get() = _uiState.asStateFlow()
+
+    init {
+        _uiState.update { currentState->
+            currentState.copy(
+                onSearchChange = {
+                    _uiState.value = _uiState.value.copy(
+                        searchText = it,
+                        searchedProducts = searchedProducts(it)
+                    )
+                }
+            )
+        }
+
+        viewModelScope.launch {
+            dao.products().collect { products ->
+                _uiState.value = _uiState.value.copy(
+                    sections = mapOf(
+                        "All products" to products,
+                        "Promotions" to sampleDrinks + sampleCandies,
+                        "Drinks" to sampleDrinks,
+                        "Candies" to sampleCandies
+                    ),
+                    searchedProducts = searchedProducts(_uiState.value.searchText)
+                )
+            }
+        }
+    }
 
     private fun containsInNameOrDescription(text: String) = { p: Product ->
         p.description?.contains(text.trim(), ignoreCase = true) == true
@@ -40,7 +59,7 @@ class HomeScreenViewModel : ViewModel() {
     private fun searchedProducts(text: String) =
         if (text.isNotBlank()) {
             listOfProducts.filter(containsInNameOrDescription(text)) +
-                    dao.products().filter(containsInNameOrDescription(text))
+                    dao.products().value.filter(containsInNameOrDescription(text))
         } else {
             emptyList()
         }
